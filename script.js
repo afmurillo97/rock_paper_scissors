85600682384aede04b56dc8e87112aa30e3ab8b6
Felipe Murillo <afmurillo97@gmail.com>
2022-03-10 21:03:04 -0500

    i have an error with playerCounter and computerCounter

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 // This function return the result of the computer.
 function computerPlay() {
     let options = ['rock', 'paper', 'scissors'];
-    let option = options[Math.floor(Math.random() * options.length)];
-    return option;
+    let optionComputer = options[Math.floor(Math.random() * options.length)];
+    return optionComputer;
 }
 // First result
 //computerPlay('');
 
 // This function return an string with the winner of one single round
 function playRound(playerSelection, computerSelection) {
-    playerSelection = prompt('Enter Rock Paper or Scissors:').toLowerCase();
-
-    computerSelection = computerPlay('');
+    computerSelection = computerPlay();
+    playerSelection = playerPlay;
     let winnerRound = '';
     if (playerSelection === computerSelection) {
         winnerRound = 'draw';
@@ -20,43 +19,46 @@ function playRound(playerSelection, computerSelection) {
                 playerSelection === 'scissors' && computerSelection === 'paper' || 
                 playerSelection === 'paper' && computerSelection === 'rock'){
                 winnerRound = 'player';
+                playerCount++;
                 return winnerRound;
     } else if (computerSelection === 'rock' && playerSelection === 'scissors' ||
                 computerSelection === 'scissors' && playerSelection === 'paper' ||
                 computerSelection === 'paper' && playerSelection === 'rock'){
-                winnerRound = 'computer'
+                winnerRound = 'computer';
+                computerCount++;
                 return winnerRound;
     } 
 
 }
-//Second result
-console.log(playRound()); 
-
 
 // This function plays 5 rounds, shows the score and choose a winner
-//  function game() {
-//     let playerCount = 0;
-//     let computerCount = 0;
-//     for (i = 1 ; i <= 5 ; i++) {
-//         winner = playRound('');
-//         if (winner === 'player') {
-//             playerCount++;
-//             console.log('You won !!! Player ' + playerCount + ' - ' + computerCount + ' Computer');
-//         } else if (winner === 'computer') {
-//             computerCount++;
-//             console.log('You lose !!! Player ' + playerCount + ' - ' + computerCount + ' Computer');
-//         } else {
-//             console.log('Draw !!! Player ' + playerCount + ' - ' + computerCount + ' Computer');
-//         }
-//     }
-//     if (playerCount > computerCount) {
-//         console.log('The winner is Player: ' + playerCount + ' - ' + computerCount);
-//     } else if (playerCount < computerCount) {
-//         console.log('The winner is Computer: ' + playerCount + ' - ' + computerCount);
-//     } else if (playerCount === computerCount) {
-//         console.log('The game is Draw: ' + playerCount + ' - ' + computerCount);
-//     }
+// function game() {
+    
+
+//Second result
+//console.log(playRound()); 
+
+// function playerPlay() {
+//     return optionPlayer;
 // }
-// Third result
-//game();
- 
+const playerOptions = document.querySelectorAll('button');
+// the .forEach method to iterate through each button
+playerOptions.forEach((button) => {
+// and for each one to add a 'click' listener
+    button.addEventListener('click', function(e){
+        playerPlay = button.id;
+        winner = playRound();
+        let playerCount = 0;
+        let computerCount = 0;
+        // if (winner === 'player') {
+        //     playerCount++;
+        //     console.log('You won !!! Player ' + playerCount + ' - ' + computerCount + ' Computer');
+        // } else if (winner === 'computer') {
+        //     computerCount++;
+        //     console.log('You lose !!! Player ' + playerCount + ' - ' + computerCount + ' Computer');
+        // } else {
+        //     console.log('Draw !!! Player ' + playerCount + ' - ' + computerCount + ' Computer');
+        // }
+    });
+
+});
